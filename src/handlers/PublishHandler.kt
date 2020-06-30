b5664b1da999ff1465dc0e543a2c34de77bf7a8d
sander høyvik <sander.hoyvik@gmail.com>
2020-06-30 23:32:31 +0200

    Update PublishHandler.kt

--- src/handlers/PublishHandler.kt
+++ src/handlers/PublishHandler.kt
@@ -6,26 +6,15 @@ import io.vertx.mqtt.messages.MqttPublishMessage
 import mu.KotlinLogging
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import wakeonlan.MachineID
-import wakeonlan.WoLClient
 
 private val logger = KotlinLogging.logger {}
 
 object SimplePublishHandler : Handler<MqttPublishMessage>, KoinComponent {
-    val machines : List<MachineID> by inject()
-    val client : WoLClient by inject()
+
 
     override fun handle(event: MqttPublishMessage?) {
         val topic = event?.topicName()?:""
         val payload = event?.payload().toString()
-
-        if(topic.isEmpty() || payload != "on") return logger.error {"Invalid message received: $payload on topic: $topic"}
-
-        val computerName = topic.split("/")[1]
-        val machine = machines.find { it.name.equals(computerName,true) }
-            ?: return logger.error { "machine with name $computerName not found"  }
-
-        client.wake(machine.macAddress,machine.host)
-        logger.info{"wake message sent to $machine"}
+        
     }
 }
